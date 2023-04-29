21fe68b3927666d4b9fc0a485891e2e10ef67154
Turasov Mikhail <miheytm@mail.ru>
2023-04-29 21:51:35 +0500

    Решили вторую Задачу

---
+++ HW002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите c: ");
+int c = int.Parse(Console.ReadLine());
+int max = a; 
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Clear();
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
