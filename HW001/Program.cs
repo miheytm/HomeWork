f7f7022ecccdd7c48622cc72049839b819435b45
Turasov Mikhail <miheytm@mail.ru>
2023-04-29 21:43:34 +0500

    add Решили первую задачу

---
+++ HW001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Clear();
+if (a > b) Console.WriteLine("a больше b");
+if (a < b) Console.WriteLine("a меньше b");
+if (a == b) Console.WriteLine("a равно b");
