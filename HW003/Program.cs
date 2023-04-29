c1b4926fcf1ab18e6c60463fbd7b63cbb492a522
Turasov Mikhail <miheytm@mail.ru>
2023-04-29 22:00:55 +0500

    Решили третью задачу

---
+++ HW003/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x == 0) 
+{
+    Console.Clear();
+    Console.WriteLine("Число Ноль"); 
+}
+else
+{
+if (x % 2 == 0)
+{
+    Console.Clear();
+    Console.WriteLine("Четное число"); 
+} 
+else
+{
+    Console.Clear();
+    Console.WriteLine("Нечетное число");  
+
+}
+}
