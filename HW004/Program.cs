ed5caea624846aa5d2576f5308f76f816ce1df34
Turasov Mikhail <miheytm@mail.ru>
2023-04-29 22:09:07 +0500

    Решили четвертую задачу

---
+++ HW004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int coint = 1;
+if (n <= 1) Console.WriteLine("Вы ввели число меньше или равное единице ");
+else
+while (coint <= n)
+{
+    if (coint % 2 == 0) Console.WriteLine(coint);
+    coint++;
+}
