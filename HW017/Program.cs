439563c880dd24a25ad901d6d1274aedf53368d7
Turasov Mikhail <miheytm@mail.ru>
2023-05-01 14:32:01 +0500

    Шестое практическое задание

---
+++ HW017/Program.cs
@@ -0,0 +1,50 @@
+﻿void InputArray(int[] arN)
+{
+    int length = arN.Length;
+    int index = 0;
+    while (index < length)
+    {
+        arN[index] = GetIntNum($"Введите {index + 1}-й элемент: ");
+        index++;
+        Console.WriteLine();
+    }
+}
+
+int GetIntNum(string mes)
+{
+    Console.Write($"{mes}: ");
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int pos = 0;
+    while (pos < count)
+    {
+        Console.Write($" {col[pos]}");
+        pos++;
+    }
+    Console.WriteLine();
+}
+
+int PlusIntArray(int[] col)
+{
+    int count = col.Length;
+    int pos = 0;
+    int result = 0;
+    while (pos < count)
+    {
+        if (col[pos] > 0) result++;
+        pos++;
+    }
+    return result;
+}
+
+Console.Clear();
+int n = GetIntNum("Введите количество элементов массива");
+int[] array = new int[n];
+InputArray(array);
+int sum = PlusIntArray(array);
+Console.WriteLine($"В массиве {sum} положительный(х) элемент(а)");
---
+++ HW018/Program.cs
@@ -0,0 +1,33 @@
+﻿int GetIntNum(string mes)
+{
+    Console.Write($"{mes}: ");
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+void GetXYLines(double c1, double a1, double c2, double a2, double b1, double b2)
+{
+if ((a1 * b2 - a2 * b1) == 0)
+{
+Console.WriteLine("Прямые параллельны другу друг (или совпадают) и не имеют точек пересечения");
+}
+else
+{
+double oX = -(c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+double oY = -(a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
+
+Console.WriteLine(oX); 
+Console.WriteLine(oY);
+}
+}
+
+Console.Clear();
+
+double c1 = GetIntNum("Введите коэффициент b1");
+double a1 = GetIntNum("Введите коэффициент k1");
+double c2 = GetIntNum("Введите коэффициент b2");
+double a2 = GetIntNum("Введите коэффициент k2");
+double b1 = -1; //коофициент y1
+double b2 = -1; //коофициент y2
+
+GetXYLines(c1, a1, c2, a2, b1, b2);
